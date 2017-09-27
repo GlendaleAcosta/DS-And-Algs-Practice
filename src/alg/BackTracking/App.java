8aefaf2ecce5947fc2e5c9c349a9f5552e567d2d
Glendale Acosta <gglendale17@gmail.com>
2017-09-27 15:29:25 -0700

    Backtracking problem with N-Queens on an NxN chessboard.

---
+++ src/alg/BackTracking/App.java
@@ -0,0 +1,8 @@
+package alg.BackTracking;
+
+public class App {
+    public static void main(String[] args) {
+        QueensProblem problem = new QueensProblem(5);
+        problem.solve();
+    }
+}
