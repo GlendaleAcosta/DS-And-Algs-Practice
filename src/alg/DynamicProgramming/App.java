cc3fe190cc41c7f3babc604964959878d6e1de27
Glendale Acosta <gglendale17@gmail.com>
2017-09-26 22:30:46 -0700

    Added Dynamic programming with fibonacci numbers.

---
+++ src/alg/DynamicProgramming/App.java
@@ -0,0 +1,8 @@
+package alg.DynamicProgramming;
+
+public class App {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        System.out.println(fibonacci.naiveFibonacci(900));
+    }
+}
---
+++ src/alg/DynamicProgramming/Fibonacci.java
@@ -0,0 +1,35 @@
+package alg.DynamicProgramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fibonacci {
+
+
+    private Map<Integer, Integer> memoizeTable; // memoize table
+
+    public Fibonacci() {
+        this.memoizeTable = new HashMap<>();
+        this.memoizeTable.put(0,0);
+        this.memoizeTable.put(1,1);
+    }
+
+    public int fibonacciDP(int n) {
+        if (memoizeTable.containsKey(n))
+            return memoizeTable.get(n);
+
+        memoizeTable.put(n-1, fibonacciDP(n-1));
+        memoizeTable.put(n-2, fibonacciDP(n-2));
+
+        int calculatedNumber = memoizeTable.get(n-1) + memoizeTable.get(n-2);
+        memoizeTable.put(n, calculatedNumber);
+        return calculatedNumber;
+    }
+
+    public int naiveFibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return naiveFibonacci(n - 1) + naiveFibonacci(n - 2);
+    }
+}
