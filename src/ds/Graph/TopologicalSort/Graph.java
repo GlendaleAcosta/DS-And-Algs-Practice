9185fa2eabe9ab233780a603bf9c9e81632899e4
Glendale Acosta <gglendale17@gmail.com>
2017-10-04 02:26:10 -0700

    Implemented topological sort.

---
+++ src/ds/Graph/TopologicalSort/Graph.java
@@ -0,0 +1,54 @@
+package ds.Graph.TopologicalSort;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Graph {
+    private int v; // # of vertices
+    private LinkedList<Integer> adj[]; // adjacency list
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for(int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    public void topologicalSort() {
+        Stack stack = new Stack();
+
+        boolean visited[] = new boolean[v];
+        for(int i = 0; i < v; i++) {
+            visited[i] = false;
+        }
+
+        for(int i = 0; i < v; i++)
+            if (visited[i] == false)
+                topologicalSortUtil(i, visited, stack);
+
+        while(!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+
+    private void topologicalSortUtil(int v, boolean visited[], Stack stack) {
+        visited[v] = true;
+        Integer i;
+
+        Iterator<Integer> it = adj[v].iterator();
+        while(it.hasNext()) {
+            i = it.next();
+            if (!visited[i])
+                topologicalSortUtil(i, visited, stack);
+        }
+
+        stack.push(new Integer(v));
+    }
+
+}
